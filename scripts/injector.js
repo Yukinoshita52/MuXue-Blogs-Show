d98871436f1720491601ca59f476a2d96cf4542f
Avidity <2642350585@qq.com>
2025-11-25 19:36:54 +0800

    Site updated: 2025-11-25 19:36:53

--- scripts/injector.js
+++
@@ -1,6 +0,0 @@
-// const js = hexo.extend.helper.get('js').bind(hexo);
-
-// hexo.extend.injector.register('head_end', () => {
-// //   return js('/js/custom.js');
-//   return js('/js/rewrite-cdn.js');
-// });
